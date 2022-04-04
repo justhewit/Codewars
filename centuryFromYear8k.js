107b336b495f8c2e3b5471aea35b1b7abc28bde1
Charlie Hewitt <charlieehewitt@gmail.com>
2022-04-04 18:39:47 -0400

    Century From Year 8kyu CW Problem

---
+++ centuryFromYear8k.js
@@ -0,0 +1,13 @@
+/*Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.*/
+
+function century(year) {
+    if(year % 100 > 0) {
+      return (Math.floor(year/100) + 1);
+    }else{
+        return year / 100
+    }
+  }
